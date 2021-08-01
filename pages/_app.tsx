bd2901eba615b32da72960e112f10a709c459259
Mika Aguilar <mika.aguilar@gmail.com>
2021-07-31 22:08:42 -0500

    feat(ga): Add goog analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 
 import 'global-styles/all.scss';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>Searching for Birds</title>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Castoro:ital@0;1&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;700&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?&family=Tangerine:wght@700&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <Component {...pageProps} />
-  </>
-);
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  return (
+    <>
+      <Head>
+        <title>Searching for Birds</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Castoro:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?&family=Tangerine:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default App;
